28cf77b3153ecd25bf526e83943dace73d6d4605
Alena <al.gnevusheva@gmail.com>
2022-11-10 12:32:28 +0400

    wip: get categories

---
+++ src/db.ts
@@ -0,0 +1,4 @@
+import firebase from 'firebase-admin'
+import { config } from './config'
+
+export const firebaseDB = firebase.initializeApp(config.firebaseConfig)
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { config } from './config';
+import { firebaseDB } from './db';
+import { loadSchema } from './utils';
+import expressPlayground from 'graphql-playground-middleware-express'
+import { resolvers } from './resolvers';
+
+
+async function start() {
+  const app = express()
+
+  const db = firebaseDB.firestore()
+  const context = { db }
+
+  const server = new ApolloServer({
+      typeDefs: loadSchema("src/schema.graphql"),
+      resolvers,
+      context
+  })
+  await server.start()
+  server.applyMiddleware({ app })
+
+  app.get('/', (req, res) => res.end('Welcome to the CrownClothingAPI'))
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+  app.listen({ port: config.port }, () =>
+      console.log(
+          `GraphQL Server running @ http://localhost:${config.port}${server.graphqlPath}`
+      )
+  )
+}
+
+start()
