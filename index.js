0d9b3479039572422996333a48252e61fce3d24c
Alena <al.gnevusheva@gmail.com>
2022-11-14 11:53:21 +0400

    fox playground and scripts

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { config } from './config.mjs';
 import { firebaseDB } from './db.mjs';
 import { loadSchema } from './utils.mjs';
 import { resolvers } from './resolvers/index.mjs';
-/* import expressPlayground from 'graphql-playground-middleware-express' */
+import expressPlayground from 'graphql-playground-middleware-express'
 
 
 async function start() {
@@ -23,7 +23,7 @@ async function start() {
   console.log(config)
 
   app.get('/', (req, res) => res.end('Welcome to the CrownClothingAPI'))
-  /* app.get('/playground', expressPlayground.default({ endpoint: '/graphql' })) */
+  app.get('/playground', expressPlayground.default({ endpoint: '/graphql' }))
   app.listen({ port: config.port }, () =>
       console.log(
           `GraphQL Server running @ http://localhost:${config.port}${server.graphqlPath}`
